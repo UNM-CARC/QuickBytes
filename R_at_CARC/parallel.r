e1d99fbad0fc47b63d00b9556fe43ebfe3afaa3d
liphardt <36514645+liphardt@users.noreply.github.com>
2019-12-11 11:36:39 -0700

    Update parallel.r
    Made the language a tad more professional

--- R_at_CARC/parallel.r
+++ R_at_CARC/parallel.r
@@ -1,6 +1,6 @@
-# This is a pointless little script to use as an example of sequential and parallel versions of the same code
+# This is a script to use as an example of sequential and parallel versions of the same code
 
-# Load the libraries you want like the usual
+# Load the libraries you want
 library(TreeSim)
 library(phangorn)
 
@@ -17,20 +17,20 @@ print(num_taxa)
 out_dir<-getwd()
 out_pre<-paste("treesims", num_taxa, lamb_var, mu_var, sep="_")
 
-# Simulate a bunch of trees cuz we can and it takes a while
+# Simulate a bunch of trees cuz because it takes a while and demonstrates speedup
 sim_trees<-sim.bd.taxa(num_taxa, 10000, lambda=lamb_var, mu=mu_var, complete=F)
 
-# cool, a bunch of trees and stuff, lets make a species tree and plot it
+# make a species tree and plot it
 
 species_tree<-speciesTree(sim_trees)
 pdf(file=paste(out_dir,"/",out_pre, ".pdf", sep=""))
 plot(species_tree)
 dev.off()
 
-# neato, lets compare the distances of all the gene trees to the species tree
+# compare the distances of all the gene trees to the species tree
 tree_distances<-lapply(sim_trees, treedist, tree2=species_tree)
 
-# Cool dude, lets calculate the mean and median of one of those things I guess
+# calculate the mean and median
 
 branch_distances<-lapply(tree_distances, getElement, name="branch.score.difference")
 mean_distance<-mean(unlist(branch_distances))
@@ -38,4 +38,4 @@ median_distance<-median(unlist(branch_distances))
 
 write(paste("for", num_taxa,"taxa, the mean distance for lambda", lamb_var, "and mu", mu_var, "is", mean_distance, sep=" "), file=paste(out_dir,"/", "distances.txt", sep=""), append=T)
 write(paste("for", num_taxa, "taxa, the median distance for lambda", lamb_var, "and mu", mu_var, "is", median_distance, sep=" "), file=paste(out_dir,"/", "distances.txt", sep=""), append=T)
-# Cool. All meaningless but whatever
+# now repeat the same code but in parellel
