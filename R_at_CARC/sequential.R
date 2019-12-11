0dcdbf2dd07e2b08a2007b3490c87ac72089b2e8
liphardt <36514645+liphardt@users.noreply.github.com>
2019-12-11 11:39:38 -0700

    Update sequential.R
    made language slightly more professional

--- R_at_CARC/sequential.R
+++ R_at_CARC/sequential.R
@@ -1,23 +1,23 @@
-# This is a pointless little script to use as an example of sequential and parallel versions of the same code
+# This is a script to use as an example of sequential and parallel versions of the same code
 
-# Load the libraries you want like the usual
+# Load the libraries
 library(TreeSim)
 library(phangorn)
 
-# Simulate a bunch of trees cuz we can and it takes a while
+# Simulate a bunch of trees
 sim_trees<-sim.bd.taxa(20, 10000, lambda=2, mu=0.5, complete=F)
 
-# cool, a bunch of trees and stuff, lets make a species tree and plot it
+# make a species tree and plot it
 
 species_tree<-speciesTree(sim_trees)
 pdf("~/wheeler-scratch/R-workshop/all_the_species_trees.pdf")
 plot(species_tree)
 dev.off()
 
-# neato, lets compare the distances of all the gene trees to the species tree
+# compare the distances of all the gene trees to the species tree
 tree_distances<-lapply(sim_trees, treedist, tree2=species_tree)
 
-# Cool dude, lets calculate the mean and median of one of those things I guess
+# calculate the mean and median
 
 branch_distances<-lapply(tree_distances, getElement, name="branch.score.difference")
 mean_distance<-mean(unlist(branch_distances))
@@ -27,4 +27,4 @@ print(mean_distance)
 print("median is")
 print(median_distance)
 
-# Cool. All meaningless but whatever
+
